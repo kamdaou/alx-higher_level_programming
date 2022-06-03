880e83ebfc012d6ea28d9aa47eee09e5b09a8a65
root <you@example.com>
2022-06-03 15:21:40 +0200

    add function that returns tuple with the length of a string and its first character

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if len_a == 0:
         tuple_a = (0, 0)
-    elif len(tuple_a) == 1:
+    elif len_a == 1:
         tuple_a = (tuple_a[0], 0)
     else:
         tuple_a = (tuple_a[0], tuple_a[1])
-    if len(tuple_b) == 0:
+    if len_b == 0:
         tuple_b = (0, 0)
-    elif len(tuple_b) == 1:
+    elif len_b == 1:
         tuple_b = (tuple_b[0], 0)
     else:
         tuple_b = (tuple_b[0], tuple_b[1])
