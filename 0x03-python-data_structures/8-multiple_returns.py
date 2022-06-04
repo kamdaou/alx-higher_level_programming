62d49763886a7d5eff205b18c0c60161f1b3e410
root <you@example.com>
2022-06-04 12:26:05 +0200

    change condition in if statement to involve some result in order to meet specificationsé

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == 0:
+    if length <= 0:
         tple = (None, 0)
     else:
         tple = (length, sentence[0])
