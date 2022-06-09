cb7e8c07bddbe846612a721fbb5ffd9a2252283d
root <you@example.com>
2022-06-09 10:32:09 +0200

    refractor the code to make sure the new_matrix has same size that the given matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 0:
-        return [[]]
-    new_matrix = []
+    new_matrix = [[None]*len(matrix[0])]*len(matrix)
     i = 0
+    j = 0
     for row in matrix:
-        new_matrix.append([])
         for element in row:
-            new_matrix[i].append(element**2)
+            new_matrix[i][j] = element**2
+            j += 1
         i += 1
+        j = 0
     return new_matrix
