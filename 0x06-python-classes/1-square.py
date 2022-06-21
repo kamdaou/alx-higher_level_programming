3fc2d82a64a94d217386e5d2b396c8ae6410bfbe
root <you@example.com>
2022-06-21 23:17:51 +0200

    add class square with private instance attribut

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Square file
+"""
+
+
+class Square:
+    """square class"""
+    pass
+
+    def __init__(self, size):
+        """init method"""
+        self.__size = size
