5c386870078078f0a0fb11d50edacc770d8c6645
root <you@example.com>
2022-06-21 23:29:22 +0200

    add class Square based on the one in 1-square.py with type and value verification

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Square file
+"""
+
+
+class Square:
+    """square class"""
+    pass
+
+    def __init__(self, size=0):
+        """init method"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
