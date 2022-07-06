c252e65727613fe718ad3848b7b1defbf9a1f683
root <you@example.com>
2022-07-06 11:33:04 +0200

    add function that Checks if the object is an instance of a class     that inherited (directly or indirectly) from the specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    Checks if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class
+    Args:
+        obj: the object
+        a_class: the class
+    Return:
+        True or False
+    """
+    if not type(obj) is a_class and issubclass(type(obj), a_class):
+        return True
+    return False
+
