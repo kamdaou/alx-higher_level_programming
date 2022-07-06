a0abe7317500489e58086d5ab9136c839f7a899d
root <you@example.com>
2022-07-06 10:59:57 +0200

    add function that checks if the object is exactly an instance of the specified class

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    Return list of attribute and method of the object
+    Args:
+        obj: the object
+    Return:
+        list of attributes and method
+    """
     return dir(obj)
